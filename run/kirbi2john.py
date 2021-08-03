2a6e224b07a4332bb5857dc82ff227a76a30976b
rgennt <kov2novych@gmail.com>
2021-08-02 20:30:52 -0400

    Bump to Python3.9

--- run/kirbi2john.py
+++ run/kirbi2john.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Based on the Kerberoast script from Tim Medin to extract the Kerberos tickets
 # from a kirbi file (https://github.com/nidem/kerberoast).
@@ -31,19 +31,17 @@ if __name__ == '__main__':
     for f in sys.argv[1:]:
         with open(f, 'rb') as fd:
             data = fd.read()
-            if data[0] == '\x76':  # process .kirbi
+            if data[0].to_bytes(1,'big') == b'\x76':  # process .kirbi
                 # rem dump
                 etype = str(decoder.decode(data)[0][2][0][3][0])
                 if etype != "23":
                     sys.stderr.write("Unsupported etype %s seen! Please report this to us.\n" % etype)
-                et = str(decoder.decode(data)[0][2][0][3][2])
-                sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + et[:16].encode("hex") +
-                                 "$" + et[16:].encode("hex") + "\n")
+                et = decoder.decode(data)[0][2][0][3][2]
+                sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + bytes(et[:16]).hex() + "$" + bytes(et[16:]).hex() + "\n")
             elif data[:2] == '6d':  # extracttgsrepfrompcap.py output
                 for ticket in data.strip().split('\n'):
                     etype = str(decoder.decode(ticket.decode('hex'))[0][4][3][0])
                     if etype != "23":
                         sys.stderr.write("Unsupported etype %s found. Such hashes can't be cracked it seems.\n" % etype)
-                    et = str(decoder.decode(ticket.decode('hex'))[0][4][3][2])
-                    sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + et[:16].encode("hex") +
-                                     "$" + et[16:].encode("hex") + "\n")
+                    et = decoder.decode(ticket.decode('hex'))[0][4][3][2]
+                    sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + bytes(et[:16]).hex() + "$" + bytes(et[16:]).hex() + "\n")
